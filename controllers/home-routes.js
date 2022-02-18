f9fbcb7f8dba434362da28c151e3fbfebf848cc3
ryanweiler92 <ryanweiler92@gmail.com>
2022-02-18 13:46:08 -0500

    login page

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -43,6 +43,15 @@ router.get('/', (req, res) => {
       });
   });
 
+  //redirect to login screen
+  router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+      res.redirect('/');
+      return;
+    }
+  
+    res.render('login');
+  });
 
 
 
