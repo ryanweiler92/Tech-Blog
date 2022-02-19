433cae4d5bf14e69d5c39ae9a83ab8cabf84b208
ryanweiler92 <ryanweiler92@gmail.com>
2022-02-19 17:55:58 -0500

    edit post

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -38,5 +38,36 @@ router.get('/', (req, res) => {
     });
 });
 
+router.get('/edit/:id', withAuth, (req, res) => {
+  Post.findByPk(req.params.id, {
+    attributes: [
+      'id',
+      'title',
+      'content',
+      'created_at',
+    ],
+    include: [
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+    .then(dbPostData => {
+      if (dbPostData) {
+        const post = dbPostData.get({ plain: true });
+        
+        res.render('edit-post', {
+          post,
+          loggedIn: true
+        });
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
 
 module.exports = router;
--- public/javascript/new-post.js
+++ public/javascript/new-post.js
@@ -5,7 +5,7 @@ function newPostFormDisplay() {
 async function newFormHandler(event) {
     event.preventDefault();
   
-    const title = document.querySelector('input[name="title"]').value;
+    const title = document.querySelector('input[name="title"]').value.trim();
     const content = document.querySelector('textarea[name="content"]').value;
   
     const response = await fetch(`/api/posts`, {
